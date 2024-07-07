37f9e74680224abb433ffa6dbd43962ce845ceeb
hantabaru1014 <hantabaru1014@gmail.com>
2024-07-08 02:57:56 +0900

    エラーページを追加

---
+++ src/routes/+error.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import SectionHeader from '$lib/components/SectionHeader.svelte';
+</script>
+
+<div>
+	{#if $page.status === 404}
+		<SectionHeader title="ご指定のページは見つかりませんでした" />
+		<p>
+			削除されたか、移動されたか、まだ作成されていない可能性があります。もう一度リンクをお確かめください。
+		</p>
+	{:else}
+		<SectionHeader title="{$page.status} - エラーが発生しました" />
+		<p>{$page.error?.message ?? 'もう一度リンクをお確かめください。'}</p>
+	{/if}
+	<div class="mt-3 flex justify-center">
+		<button on:click={() => history.back()}>戻る</button>
+	</div>
+</div>
